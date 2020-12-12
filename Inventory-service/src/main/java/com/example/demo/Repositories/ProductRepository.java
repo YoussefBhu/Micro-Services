a224b0f83b1321f756ac4116335ab89ef95e6855
Bichou123 <youssef.bichouu@gmail.com>
2020-12-12 20:25:10 +0100

    first

---
+++ Inventory-service/src/main/java/com/example/demo/Repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.Repositories;
+
+import com.example.demo.Entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
